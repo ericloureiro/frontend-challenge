b36ab3b695e772a34be8a23787d534f41bed2c7f
Eric Loureiro <ericloureiro@gmail.com>
2026-04-21 11:54:38 -0300

    chore: update styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,22 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { ReduxProvider } from "../store";
+import type { Metadata } from "next";
+import { PropsWithChildren } from "react";
+import { Header } from "@/components";
+import { ReduxProvider } from "@/store";
 
 export const metadata: Metadata = {
   title: "ESL FACEIT",
   description: "Senior Frontend Challenge",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang="en">
-      <body>
-        <ReduxProvider>{children}</ReduxProvider>
+      <body className="h-screen flex flex-col">
+        <Header />
+        <main className="h-full flex flex-1 justify-center p-8">
+          <ReduxProvider>{children}</ReduxProvider>
+        </main>
       </body>
     </html>
   );
